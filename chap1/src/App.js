d851b3e1a9f6e9b686e71e9c75eb532fdf77302c
Fawad Iqbal <fawadeqbal@yahoo.com>
2023-03-03 23:35:31 +0500

    chap1

--- chap1/src/App.js
+++ chap1/src/App.js
@@ -1,7 +1,7 @@
-import Header from './Header';
-import Content from './Content';
-import Footer from './Footer';
-import './index.css'
+import Header from './components/Header';
+import Content from './components/Content';
+import Footer from './components/Footer';
+import './css/index.css'
 
 function App() {
 
