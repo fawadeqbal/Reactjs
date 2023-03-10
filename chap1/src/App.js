c9f31060c7d2dad8dfe62dd1236bc7afb902441a
fawadeqbal <fawadeqbal@yahoo.com>
2023-03-10 16:00:11 +0500

    Added

--- chap1/src/App.js
+++ chap1/src/App.js
@@ -1,7 +1,6 @@
 import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
-import './css/index.css'
 
 function App() {
 
--- chap1/src/components/Content.js
+++ chap1/src/components/Content.js
@@ -1,18 +1,50 @@
 import React from 'react'
+import { useState } from 'react'
+import {FaTrashAlt} from 'react-icons/fa'
 
 const Content = () => {
-    const handleNameChange = () => {
-        const names = ['Fawad' , 'Faizan' , 'Yaman']
-        const int = Math.floor(Math.random()*3)
-        return names[int]
-      }
+
+      const [items, setItems] = useState([
+        {
+          id: 1,
+          checked: false,
+          item: "One half pound bag of cocoa covered Almonds Unsalted"
+        },
+        {
+          id: 2,
+          checked: false,
+          item: "Item 2"
+        },
+        {
+          id: 3,
+          checked: false,
+          item: "Item 3"
+        }
+      ]);
+
   return (
     <main>
-        <p>
-         Hello {handleNameChange()}!
-        </p>
+        <ul>
+          {items.map((item)=> (
+            <li className='item' key= {item.id}>
+              <input
+                type='checkbox'
+                checked={item.checked}
+              />
+              <label>{item.item}</label>
+              <FaTrashAlt 
+                role='button' 
+                tabIndex='0'
+              />
+            </li>
+          ))}
+        </ul>
     </main>
   )
 }
 
 export default Content
+
+
+
+      
